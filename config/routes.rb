ba1b8b159f3e3802b87cb2b02d86ec725d51f88d
Fatima Najafi <najafifatima762@gmail.com>
2023-11-29 01:31:29 +0430

    Upate the cors.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  namespace :api do
+    get 'greetings/random', to: 'greetings#random'
+  end
+
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
