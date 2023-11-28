b9a1f89c2e674532e219637d098274f3ea902c06
Fatima Najafi <najafifatima762@gmail.com>
2023-11-29 01:31:53 +0430

    Create geeting table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Message.create(message: 'Hello! 😊 I hope you are having a fantastic day filled with joy and productivity.')
+Message.create(message: 'Greetings! 🌞 Wishing you a warm and wonderful day ahead. May it be filled with positivity and success')
+Message.create(message: 'Welcome to my app!')
+Message.create(message: 'Hi! 🌼 Just wanted to drop by and say hello. I hope your day is as amazing as you are!')
+Message.create(message: 'Good morning! ☀️ Rise and shine! Wishing you a day filled with happiness and achievements')
